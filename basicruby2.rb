77f87ea5492caf90d427cbaac901f77b7475173e
John LaGreco <johnlagreco@gmail.com>
2015-12-22 00:22:49 -0500

    Initial commit

---
+++ basicruby2.rb
@@ -0,0 +1,8 @@
+# Start by storing the text within a hash where each 
+# word is a key with the word's value being the amount 
+# of times it has been used in the block:
+# ruby {the: 10, john: 1, of: 15}
+
+# Once you've created this hash, 
+# return the word that has been used the most
+
