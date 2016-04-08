86d138952540a1cb274c5aea8ab69b11aad425de
John LaGreco <johnlagreco@gmail.com>
2016-04-08 18:41:03 -0400

    Added ReadMe file.

--- basicruby2.rb
+++ basicruby2.rb
@@ -1,17 +1,3 @@
-# Create a program to analyze 
-# a large block of text and report 
-# back on the frequency of each word in the text.
-
-# Start by storing the text within a hash where each 
-# word is a key with the word's value being the amount 
-# of times it has been used in the block:
-# ruby {the: 10, john: 1, of: 15}
-
-# Once you've created this hash, 
-# return the word that has been used the most
-
-# When you're done, encapsulate your script inside of a method that can analyze any block of text fed to it
-
 large_block = "This is my content. There are many like it, but this one is mine.My content is my best friend. It is my life. I must master it as I must master mylife.My content, without me, is useless. Without my content, I am useless. I must fire my content true. My content is human, even as I, because it is my life. Thus, I will learn it as a brother. I will learn its weaknesses, its strength, its parts, its accessories, its sights and its barrel. I will keep my content clean and ready, even as I am clean and ready. We will become part of each other. We will..."
 
 # This is the large block of text that we are going to run the program through to find the word with the most amount of occurences.
